293f4a7b36727e2e5dc69c624ea5aba781580b47
marcosfeijoo1997 <marcosfeijoo97@gmail.com>
2023-09-07 02:03:03 -0300

    agregado de hamburguer y responsividad

--- src/Componentes/Navbar/Navbar.js
+++ src/Componentes/Navbar/Navbar.js
@@ -13,12 +13,20 @@ import { useState } from 'react';
 const Navbar = () => {
 const {setSearch}=useSearchContext()
 
+  const [selectedMenu, setSelectedMenu] = useState(null);
 
-const [isOpen, setIsOpen] = useState(false);
+  const handleButtonClick = (menuNumber) => {
+    setSelectedMenu(menuNumber);
+    if (menuNumber===selectedMenu){
+   setSelectedMenu(null)
+    }
+  };
 
-const toggleOpen = () => {
-  setIsOpen(!isOpen);
-};
+
+
+
+
+
     return (
         <div className="Navbar">
             <div className="PrimerBar">
@@ -37,11 +45,13 @@ const toggleOpen = () => {
     <ul id="menu" className='menu'>
     <div className="dropdown elemento-desplegable">
                                   <NavLink to="/Category/ferreteria">
-  <button  onClick={toggleOpen} className={`dropdown-toggle ${isOpen ? 'open' : ''}`} >
+  <button  className='dropdown-toggle' onClick={() => handleButtonClick(1)} >
 Ferreteria
   </button>
+  
   </NavLink>
-  {isOpen && (
+  {selectedMenu === 1 && (
+  
   <div aria-labelledby="dropdownMenuButton">
       
   <Link to="/Category/ferreteria/bocallaves"><p className="dropdown-item">Set de bocallaves</p></Link>
@@ -54,14 +64,15 @@ Ferreteria
 <div className="dropdown">
                         <NavLink to="/Category/maquinas">
                
-  <button  onClick={toggleOpen} className={`dropdown-toggle ${isOpen ? 'open' : ''}`} >
+  <button className='dropdown-toggle' onClick={() => handleButtonClick(2)}>
 Máquinas
   </button>  </NavLink>
+  {selectedMenu === 2 && (
     <div  aria-labelledby="dropdownMenuButton">
     <Link to="/Category/maquinas/taladros" className='Link'> <p className="dropdown-item" href="#">Taladros</p></Link>
   <Link to="/Category/maquinas/amoladoras">    <p className="dropdown-item" href="#">Amoladoras</p></Link>
   <Link to="/Category/maquinas/soldadoras">  <p className="dropdown-item" href="#">Soldadoras</p></Link>
-  </div>
+  </div>)}
 
                       </div>
                       <div className="dropdown">
@@ -69,31 +80,33 @@ Máquinas
                     <NavLink to="/Category/electricidad">
                
                   
-<button className=" dropdown-toggle" >
+<button className="dropdown-toggle" onClick={() => handleButtonClick(3)}>
 Electricidad
-</button>     </NavLink>
-<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+</button>     </NavLink>  {selectedMenu === 3 && (
+
+<div aria-labelledby="dropdownMenuButton">
 <Link to="/Category/electricidad/tomas">  <p className="dropdown-item" href="#">Tomas</p></Link>
 <Link to="/Category/electricidad/cables"> <p className="dropdown-item" href="#">Cables</p></Link>
 <Link to="/Category/electricidad/luces">   <p className="dropdown-item" href="#">Luces</p></Link>
 </div>
-
+  )     }   
 
 
                
                     </div>
-                    <div className="dropdown">
+                              <div className="dropdown">
                 
                 <NavLink to="/Category/pinturas">
         
-<button className=" dropdown-toggle" >
+<button className="dropdown-toggle" onClick={() => handleButtonClick(4)}>
 Pintureria
 </button>       </NavLink>
-<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+{selectedMenu===4&&(
+<div aria-labelledby="dropdownMenuButton">
 <p className="dropdown-item" href="#">Esmaltes sinteticos</p>
 <p className="dropdown-item" href="#">Pinturas latex</p>
 <p className="dropdown-item" href="#">Barnices</p>
-</div>
+</div>)}
 </div>
                   
          
@@ -101,14 +114,16 @@ Pintureria
         
                 <NavLink>
           
-<button className=" dropdown-toggle" >
+<button className=" dropdown-toggle" onClick={() => handleButtonClick(5)}>
 Seguridad
 </button>       </NavLink>
-<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+{selectedMenu===5&&(
+<div aria-labelledby="dropdownMenuButton">
 <p className="dropdown-item" href="#">Proteccion ocular</p>
 <p className="dropdown-item" href="#">Indumentaria</p>
 
-</div>
+</div>)}
 </div>
                 
     
